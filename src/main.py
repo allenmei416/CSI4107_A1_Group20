800edf1a024e08b18c0daf35cc1468cc9feb0171
allenmei416 <allenmei416@gmail.com>
2025-03-25 17:19:50 -0400

    update naming

--- src/main.py
+++ src/main.py
@@ -4,16 +4,16 @@ from elasticsearch import Elasticsearch
 #import preprocessing as preprocess
 import indexing as index
 import retrieval as retrieval
-import rerank_BERT2 as rerank_BERT
-import rerank_FAISS as rerank_FAISS
+import rerank_BERT as rerank_BERT
 import rerank_cross_encoder as rerank_cross_encoder
+import rerank_BERT_BM25 as rerank_BERT_BM25
 
 run_name = "test_run"
 corpus_file = "../data/corpus.jsonl"
 query_file = "../data/queries.jsonl"
 result_file = "../data/results/results.txt"
 result_file_bert = "../data/results/results_bert.txt"
-result_file_faiss = "../data/results/results_faiss.txt"
+result_file_bert_BM25 = "../data/results/results_bert_BM25.txt"
 result_file_cross_encoder = "../data/results/results_cross_encoder.txt"
 output_file = "../data/corpus_preprocessed.jsonl"
 index_name = "inverted_index"
@@ -32,11 +32,14 @@ es = Elasticsearch("http://localhost:9200")
 # retrieval.run_queries(es, run_name, query_file, result_file, index_name)
 
 # 4 - reranking with BERT SentenceTransformer
+# print("Running queries...")
+# rerank_BERT.run_queries_BERT(es, run_name, query_file, result_file_bert, index_name)
+
+
+# 5 - reranking with Cross Encoder
+# print("Running queries...")
+# rerank_cross_encoder.run_queries_cross_encoder(es, run_name, query_file, result_file_cross_encoder, index_name)
+
+# 6 - reranking with BERT SentenceTransformer and BM25
 print("Running queries...")
-rerank_BERT.run_queries_BERT(es, run_name, query_file, result_file_bert, index_name)
-
-
-# 5 - reranking with FAISS
-# rerank_FAISS.run_queries_FAISS(es, run_name, query_file, result_file_faiss, index_name)
-
-#rerank_cross_encoder.run_queries_cross_encoder(es, run_name, query_file, result_file_cross_encoder, index_name)
+rerank_BERT_BM25.run_queries_BERT(es, run_name, query_file, result_file_bert_BM25, index_name)
